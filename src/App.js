6d1846801c594f38105cd7c2e39cf845ce61ffda
nasimqureshi <qureshi.nasim@yahoo.com>
2024-06-15 11:48:10 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-
-import { useState } from 'react';
+import { Header } from "./components/Header";
+import { TaskList } from "./components/TaskList";
 import './App.css';
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {id: 5271, name:"Record React Lectures", completed: true},
-    {id: 7825, name:"Edit react Lectures", completed: false},
-    {id: 8391, name: "Watch Lectures", completed: false}
-  ]);
-
-  const [show, setShow] = useState(false);
-
-  function handleDelete(id){
-    setTasks(tasks.filter(task => task.id !== id))
-  }
 
   return (
     <div className="App">
-      <h1>Task List</h1>
-      <ul>
-        <button className='trigger' onClick={() =>setShow(!show)}>Toggle</button>
-        { show && tasks.map((task) => (
-          <li key={task.id} className={task.completed ? "completed" : "incomplete"}>
-            <span>{task.id} - {task.name}</span>
-            <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
-          </li>
-        ))}
-      </ul>
+      <Header />
+      <TaskList />
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+import Logo from "../assets/logo2.png"
+
+export const Header = () => {
+  return (
+    <header>
+        <img src={Logo} alt="" />
+        <a href="/">Home</a>
+    </header>
+  )
+}
